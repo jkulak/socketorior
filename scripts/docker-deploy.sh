1bd039a72842268873127b0469b0ae9f1fce7d37
Jakub Kułak <jakub.kulak@gmail.com>
2016-11-26 19:39:37 -0600

    Fix deployment script

--- scripts/docker-deploy.sh
+++ scripts/docker-deploy.sh
@@ -5,9 +5,9 @@ docker push jkulak/socketorior
 ssh -oStrictHostKeyChecking=no deploy@$DEPLOY_HOST << EOF
 
     docker pull jkulak/socketorior:latest
-    docker stop web || true
-    docker rm web || true
+    docker stop socketorior || true
+    docker rm socketorior || true
     docker rmi jkulak/socketorior:current || true
     docker tag jkulak/socketorior:latest jkulak/socketorior:current
-    docker run -d --restart always --name socketorior-web -p 20207:8080 jkulak/socketorior:current
+    docker run -d --restart always --name socketorior -p 20207:8080 jkulak/socketorior:current
 EOF
